2987b6dec6fbe700489451d0636e147b19096cba
RyanFlachman <ryan@flachman.net>
2019-04-15 09:21:43 -0600

    Add data model

--- main.js
+++ main.js
@@ -10,20 +10,159 @@ var taskBtn = document.querySelector('.task-form__make-task-btn');
 var clearBtn = document.querySelector('.task-form__clear-btn')
 var filterBtn = document.querySelector('.filter-div__filter-btn');
 var taskList = document.querySelector('.task-form__task-list')
+var mainSection = document.querySelector('.main__section')
 
 /*-------------Global Variables-----------*/
 
-var taskArray = [];
+var taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
+var currentList = JSON.parse(localStorage.getItem('currentList')) || [];
 
 /*--------------On page Load---------------*/
 
+if (currentList.length != 0) {
+  loadCurrentList(currentList)
+}
+if(taskArray.length != 0) {
+  loadTaskList(taskArray)
+  cardOnPageLoad(taskArray)
+}
+
 /*--------------Event Listeners-------------*/
+
+asideForm.addEventListener('click', function(e) {
+  e.preventDefault()
+  if (e.target.className.includes('task-item__item-btn')) {
+    createNewTask(currentList, 'currentList')
+  }
+  if (e.target.className.includes('task-form__make-task-btn')) {
+    updateTaskArray()
+    createTaskCard(taskArray, 'currentList')
+    console.log(taskArray)
+  }
+  if (e.target.className.includes('task-form__clear-btn')) {
+    clearAll()
+  }
+})
 
 /*-----------------functions----------------*/
 
-const createTask = (taskList, titleInput) => {
-  var newList = new ToDoList(Date.now(), titleInput.value, false, taskList.value)
-  taskArray.push(newList)
+function makeNewTask() {
+  var newList = new ToDoList(Date.now(), titleInput.value, false)
+  var task = {
+    id: Date.now(),
+    complete: false,
+    toDo: itemInput.value
+  }
+  newList.tasks.push(task)
+  currentList.push(newList)
 }
+
+function updateNewTask(array) {
+  var newArray = array.map(item => {
+    item.title = titleInput.value;
+    var task = {
+      id: Date.now(),
+      complete: false,
+      toDo: itemInput.value
+    }
+    item.tasks.push(task);
+  })
+  array = newArray
+}
+
+function createNewTask(array, key) {
+  if (array.length != 0) {
+    updateNewTask(array)
+  } else {
+    makeNewTask(key)
+  }
+  displayTask(array)
+  array[0].saveToStorage(key, currentList);
+  itemInput.value = '';
+  console.log(localStorage)
+}
+
+function loadCurrentList(array) {
+  var newArray = array.map(item => {
+    item = new ToDoList(item.id, item.title, item.urgent, item.tasks)
+    return item
+  })
+  currentList = newArray;
+  displayTask(currentList)
+}
+
+function loadTaskList(array) {
+  var newArray = array.map(item => {
+    item = new ToDoList(item.id, item.title, item.urgent, item.tasks, item.taskList)
+    return item
+  })
+  taskArray = newArray;
+}
+
+function displayTask(array) {
+  taskList.innerHTML = '';
+  array[0].tasks.forEach((item) => {
+    taskList.innerHTML += 
+    `<li class="task-list__list-item">${item.toDo}</li>`;
+  })
+  titleInput.value = array[0].title
+}
+
+function createTaskCard(array, key) {
+  mainSection.innerHTML = '';
+  array.forEach(item => {
+    item.updateToDo()
+    createCard(item)
+  })
+  localStorage.removeItem(key)
+  clearAll()
+}
+
+function cardOnPageLoad(array) {
+  array.forEach(item => {
+    item.updateToDo()
+    createCard(item)
+  })
+}
+
+function clearAll() {
+  localStorage.removeItem('currentList')
+  currentList = []
+  titleInput.value = '';
+  itemInput.value = '';
+  taskList.innerHTML = '';
+
+}
+
+function createCard(task) {
+  mainSection.innerHTML += 
+  `<div class="section__task-card" data-id="${task.id}">
+    <article class="task-card__card-header">
+     <h2 class="card-header__task-title">${task.title}</h2>
+   </article>
+   <article class="task-card__card-body">
+      <ul class="card-body__task-list">${task.taskList}</ul>
+   </article>
+   <article class="task-card__card-footer">
+      <button class="card-footer__urgent-btn"></button>
+      <button class="card-footer__delete-btn"></button>
+   </article>
+  </div>
+`
+}
+
+function updateTaskArray() {
+  taskArray.push(currentList[0])
+  taskArray[0].saveToStorage('taskArray', taskArray)
+  console.log(taskArray)
+}
+
+function removeAll() {
+  localStorage.clear()
+  taskArray = [];
+  currentList = [];
+}
+
+
 
 
--- todo-list.js
+++ todo-list.js
@@ -1,24 +1,35 @@
  class ToDoList {
-   contructor(id, title, urgent, tasks) {
+  constructor(id, title, urgent, tasks, taskList) {
     this.id = id;
     this.title = title;
-    this.urgent = false;
-    this.tasks = [];
-   }
+    this.urgent = urgent || false;
+    this.tasks = tasks || [];
+    this.taskList = taskList || '';
+  };
 
-   saveToStorage() {
+  saveToStorage(array, object) {
+    var stringified = JSON.stringify(object);
+	  localStorage.setItem(array, stringified);
+  };
 
-   };
+  deleteFromStorage(array) {
+    var parsed = JSON.parse(localStorage.getItem)
 
-   deleteFromStorage() {
+  };
 
-   }
+  updateToDo() {
+    this.taskList = []
+    this.tasks.map((item) => {
+      this.taskList += 
+      `<li class="task-list__list-item" contenteditable="true">${item.toDo}</li>`;
+    })
+  };
 
-   updateToDo() {
+  updateTask() {
+    this.tasks.map((item) => {
+      this.taskList += 
+      `<li class="task-list__list-item">${item}</li>`;
+    })
+  };
 
-   }
-
-   updateTask() {
-     
-   }
- }
+};
